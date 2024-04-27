79b33edb0a9aa02aa0d657ea6c20dd68f88297f9
KBH00 <wlfjddlgoqn@knu.ac.kr>
2024-04-27 18:11:34 +0900

    edit text1

--- ai/llm/inference.py
+++ ai/llm/inference.py
@@ -50,37 +50,15 @@ class llama2_chain:
         return llm_chain, prompt
     
 
-model_paths = [#"C:/Users/kbh/Code/models/llama-2-7b-chat.Q2_K.gguf",
-               #"C:/Users/kbh/Code/models/llama-2-13b-chat.Q3_K_M.gguf",
-               "C:/Users/kbh/Code/models/llama-2-13b-chat.Q4_K_M.gguf",
-               "C:/Users/kbh/Code/models/llama-2-13b-chat.Q5_K_M.gguf",
-               "C:/Users/kbh/Code/models/llama-2-13b-chat.Q8_0.gguf"]
+model_paths = ["C:/Users/kbh/Code/models/llama-2-7b-chat.Q2_K.gguf",]
+            #    "C:/Users/kbh/Code/models/llama-2-13b-chat.Q3_K_M.gguf",
+            #    "C:/Users/kbh/Code/models/llama-2-13b-chat.Q4_K_M.gguf",
+            #    "C:/Users/kbh/Code/models/llama-2-13b-chat.Q5_K_M.gguf",
+            #    "C:/Users/kbh/Code/models/llama-2-13b-chat.Q8_0.gguf"]
 n_batches = [2000, 3000, 4000]
 n_gpu = 50
-import time
+llm = llama2_chain(model_paths, n_batch=n_batches[0], n_gpu_layers=n_gpu)
+llm_chain, prompt = llm.llm_set()
+response = llm_chain.invoke(prompt)
 
-f=open('C:/Users/kbh/Code/expr.txt', 'w')
-
-for path in model_paths:
-    for n_batch in n_batches:
-        llm = llama2_chain(path, n_batch=n_batch, n_gpu_layers=n_gpu)
-        llm_chain, prompt = llm.llm_set()
-        cnt=0
-        avg_time = 0
-        for i in range(100):
-            start_time = time.time()
-            response = llm_chain.invoke(prompt)
-            end_time = time.time()
-            if response['text'].find('Title') != -1 and response['text'].find('Authors') and response['text'].find('Abstract'):
-                cnt+=1
-            if i!=0:
-                curr_time = end_time-start_time
-                avg_time += curr_time
-        f.write('===============================\n')
-        f.write('model : %s\n' %path)
-        f.write('batch : %d\n' %n_batch)
-        f.write('acc : %d%%\n' %cnt)
-        f.write('time : %.2f\n' %avg_time)
-        f.write('===============================\n\n')
-        f.flush()
     
