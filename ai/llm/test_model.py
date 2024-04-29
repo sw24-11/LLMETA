34cb22537366fd296b49092c7317b0847a529845
KBH00 <wlfjddlgoqn@knu.ac.kr>
2024-04-30 05:41:16 +0900

    llm testing and extraction1

--- ai/llm/test_model.py
+++ ai/llm/test_model.py
@@ -1,6 +1,58 @@
 import time
 
 f=open('C:/Users/kbh/Code/expr.txt', 'w')
+class llama2_chain:
+
+    def __init__(self, model_path, n_gpu_layers, n_batch):
+        self.model_path=model_path
+        self.n_gpu_layers=n_gpu_layers
+        self.n_batch = n_batch
+    
+    def llm_set(self):
+        metadata_extraction_template = """
+        Given the following Actual Text, extract the title, authors, abstract and research domain. 
+        and do not say anything else.
+
+        Actual Text: {text}
+
+        """
+
+        prompt_template = PromptTemplate(template=metadata_extraction_template, input_variables=["text"])
+
+        #callback_manager = CallbackManager([StreamingStdOutCallbackHandler()])
+        callback_manager = CallbackManager([])
+
+        llm = LlamaCpp(
+            model_path=self.model_path,
+            n_gpu_layers=self.n_gpu_layers,
+            n_batch=self.n_batch,
+            #temperature=0,  
+            callback_manager=callback_manager,
+            verbose=False,
+            n_ctx=3000
+        )
+
+        llm_chain = LLMChain(prompt=prompt_template, llm=llm)
+
+        input_text = """
+        Autoencoders
+        Dor Bank, Noam Koenigstein, Raja Giryes
+        Abstract An autoencoder is a specific type of a neural network, which is mainly
+        designed to encode the input into a compressed and meaningful representation, and
+        then decode it back such that the reconstructed input is similar as possible to the
+        original one. This chapter surveys the different types of autoencoders that are mainly
+        used today. It also describes various applications and use-cases of autoencoders.
+        """
+
+        prompt = prompt_template.format(text=input_text)
+        return llm_chain, prompt
+model_paths = ["C:/Users/kbh/Code/project2/llm/models/llama-2-7b-chat.Q2_K.gguf",]
+            #    "C:/Users/kbh/Code/models/llama-2-13b-chat.Q3_K_M.gguf",
+            #    "C:/Users/kbh/Code/models/llama-2-13b-chat.Q4_K_M.gguf",
+            #    "C:/Users/kbh/Code/models/llama-2-13b-chat.Q5_K_M.gguf",
+            #    "C:/Users/kbh/Code/models/llama-2-13b-chat.Q8_0.gguf"]
+n_batches = [2000, 3000, 4000]
+n_gpu = 50
 
 for path in model_paths:
     for n_batch in n_batches:
