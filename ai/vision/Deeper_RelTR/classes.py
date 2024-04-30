7e3c03b74299cc39d74ff8e836d28c173f650b0c
KBH00 <wlfjddlgoqn@knu.ac.kr>
2024-05-01 01:51:45 +0900

    class post-processing with efficient1

--- ai/vision/Deeper_RelTR/classes.py
+++ ai/vision/Deeper_RelTR/classes.py
@@ -1,10 +1,50 @@
-labels = "bear, elephant, shark, snake, reptile, amphibian, goldfish, tiger shark, hammerhead, electric ray, stingray, cock, hen, ostrich, European fire salamander, common newt, eft, spotted salamander, axolotl, bullfrog, tree frog, tailed frog, loggerhead, leatherback turtle, mud turtle, terrapin, box turtle, banded gecko, common iguana, American chameleon, whiptail, agama, frilled lizard, alligator lizard, Gila monster, green lizard, African chameleon, Komodo dragon, African crocodile, American alligator, triceratops, thunder snake, ringneck snake, hognose snake, green snake, king snake, garter snake, water snake, vine snake, night snake, boa constrictor, rock python, Indian cobra, green mamba, sea snake, horned viper, diamondback, sidewinder, jellyfish, sea anemone, brain coral, flatworm, nematode, conch, snail, slug, sea slug, chiton, chambered nautilus, Dungeness crab, rock crab, fiddler crab, king crab, American lobster, spiny lobster, crayfish, hermit crab, isopod, tusker, echidna, platypus, wallaby, koala, wombat, grey whale, killer whale, dugong, sea lion, timber wolf, white wolf, red wolf, coyote, dingo, dhole, African hunting dog, hyena, red fox, kit fox, Arctic fox, grey fox, cougar, lynx, leopard, snow leopard, jaguar, lion, tiger, cheetah, mongoose, meerkat, tiger beetle, ladybug, ground beetle, long-horned beetle, leaf beetle, dung beetle, rhinoceros beetle, weevil, fly, bee, ant, grasshopper, cricket, walking stick, cockroach, mantis, cicada, leafhopper, lacewing, dragonfly, damselfly, monarch, cabbage butterfly, sulphur butterfly, lycaenid, starfish, sea urchin, sea cucumber, wood rabbit, hare, Angora, hamster, porcupine, fox squirrel, marmot, beaver, guinea pig, sorrel, zebra, hog, wild boar, warthog, hippopotamus, ox, water buffalo, bison, ram, bighorn, ibex, hartebeest, impala, gazelle, Arabian camel, llama, weasel, mink, polecat, black-footed ferret, otter, skunk, badger, armadillo, three-toed sloth, orangutan, gorilla, chimpanzee, gibbon, siamang, guenon, patas, baboon, macaque, langur, colobus, proboscis monkey, marmoset, capuchin, howler monkey, titi, spider monkey, squirrel monkey, Madagascar cat, indri."
-labels_list = labels.split(',')
+CLASSES = [ 'N/A', 'airplane', 'animal', 'arm', 'bag', 'banana', 'basket', 'beach', 'bear', 'bed', 'bench', 'bike',
+            'bird', 'board', 'boat', 'book', 'boot', 'bottle', 'bowl', 'box', 'boy', 'branch', 'building',
+            'bus', 'cabinet', 'cap', 'car', 'cat', 'chair', 'child', 'clock', 'coat', 'counter', 'cow', 'cup',
+            'curtain', 'desk', 'dog', 'door', 'drawer', 'ear', 'elephant', 'engine', 'eye', 'face', 'fence',
+            'finger', 'flag', 'flower', 'food', 'fork', 'fruit', 'giraffe', 'girl', 'glass', 'glove', 'guy',
+            'hair', 'hand', 'handle', 'hat', 'head', 'helmet', 'hill', 'horse', 'house', 'jacket', 'jean',
+            'kid', 'kite', 'lady', 'lamp', 'laptop', 'leaf', 'leg', 'letter', 'light', 'logo', 'man', 'men',
+            'motorcycle', 'mountain', 'mouth', 'neck', 'nose', 'number', 'orange', 'pant', 'paper', 'paw',
+            'people', 'person', 'phone', 'pillow', 'pizza', 'plane', 'plant', 'plate', 'player', 'pole', 'post',
+            'pot', 'racket', 'railing', 'rock', 'roof', 'room', 'screen', 'seat', 'sheep', 'shelf', 'shirt',
+            'shoe', 'short', 'sidewalk', 'sign', 'sink', 'skateboard', 'ski', 'skier', 'sneaker', 'snow',
+            'sock', 'stand', 'street', 'surfboard', 'table', 'tail', 'tie', 'tile', 'tire', 'toilet', 'towel',
+            'tower', 'track', 'train', 'tree', 'truck', 'trunk', 'umbrella', 'vase', 'vegetable', 'vehicle',
+            'wave', 'wheel', 'window', 'windshield', 'wing', 'wire', 'woman', 'zebra']
+
+INTERSECTION_CLASSES = ['pizza', 'ski', 'plate', 'racket', 'vase', 'jean', 'screen', 'umbrella', 'pillow', 'wing', 
+                            'banana', 'kite', 'plane', 'laptop', 'zebra', 'pole', 'desk', 'sock', 'pot', 'ear', 'orange', 'cup']
+
+PERSON_CLASSES = ['guy', 'girl', 'child', 'boy', 'kid', 'lady', 'man', 'men', 'people', 'person', 'player', 'woman']
+
+#PERSON_CLASSES = ['guy', 'girl', 'child', 'boy', 'kid', 'lady', 'man', 'men', 'people', 'person', 'player', 'woman',
+    #                   'eye', 'ear', 'face', 'head', 'hair','mouth', 'neck', 'nose', 'arm']
+
+# BREEDS_CLASSES = ['animal','bag','bird', 'bear', 'cow', 'giraffe', 'horse', 'sheep',
+#                     'bus','car','cap','chair','elephant','wheel','window', 'dog', 'vehicle', 'phone']
+
+second_classes = ['bird', 'cat', 'dog', 'bear', 'elephant', 'boat', 'bus', 'car', 'train', 
+                    'truck', 'bottle', 'chair', 'clock', 'laptop', 'phone', 'tie', 'wheel',
+                    'flower', 'fruit', 'vegetable', 'vehicle', 'animal']
+
+need_more_classes = second_classes + PERSON_CLASSES
+
+REL_CLASSES = ['__background__', 'above', 'across', 'against', 'along', 'and', 'at', 'attached to', 'behind',
+            'belonging to', 'between', 'carrying', 'covered in', 'covering', 'eating', 'flying in', 'for',
+            'from', 'growing on', 'hanging from', 'has', 'holding', 'in', 'in front of', 'laying on',
+            'looking at', 'lying on', 'made of', 'mounted on', 'near', 'of', 'on', 'on back of', 'over',
+            'painted on', 'parked on', 'part of', 'playing', 'riding', 'says', 'sitting on', 'standing on',
+            'to', 'under', 'using', 'walking in', 'walking on', 'watching', 'wearing', 'wears', 'with']
 
 cat_classes = [
     "Tabby", "Tiger cat", "Persian cat", "Siamese cat", "Egyptian cat",
     "Cougar", "Lynx", "Leopard", "Snow leopard", "Jaguar", "Lion", "Tiger", "Cheetah"
 ]
+
+bear_classes = ['bear', 'polar bear', 'American black bear', 'ice bear', 'sloth bear']
+elephant_classes = ['elephant', 'African elephant', 'Indian elephant']
+
 dog_classes = [
     "Chihuahua", "Japanese spaniel", "Maltese dog", "Pekinese", "Shih-Tzu", "Blenheim spaniel", "Papillon", "Toy terrier",
     "Rhodesian ridgeback", "Afghan hound", "Basset", "Beagle", "Bloodhound", "Bluetick", "Black-and-tan coonhound", "Walker hound",
@@ -29,8 +69,6 @@ bird_classes = [
 ]
 
 animal_classes = [
-    'bear', 'polar bear', 'American black bear', 'ice bear', 'sloth bear',
-    'elephant', 'African elephant', 'Indian elephant',
     'shark', 'goldfish', 'tiger shark', 'hammerhead', 'electric ray', 'stingray',
     'snake', 'thunder snake', 'ringneck snake', 'hognose snake', 'green snake', 'king snake', 'garter snake', 'water snake', 'vine snake', 'night snake', 'boa constrictor', 'rock python', 'Indian cobra', 'green mamba', 'sea snake', 'horned viper', 'diamondback', 'sidewinder',
     'reptile', 'banded gecko', 'common iguana', 'American chameleon', 'whiptail', 'agama', 'frilled lizard', 'alligator lizard', 'Gila monster', 'green lizard', 'African chameleon', 'Komodo dragon', 'African crocodile', 'American alligator',
@@ -55,16 +93,21 @@ animal_classes = [
     'primate', 'orangutan', 'gorilla', 'chimpanzee', 'gibbon', 'siamang', 'guenon', 'patas', 'baboon', 'macaque', 'langur', 'colobus', 'proboscis monkey', 'marmoset', 'capuchin', 'howler monkey', 'titi', 'spider monkey', 'squirrel monkey', 'Madagascar cat', 'indri'
 ]
 
-# List of subclasses to exclude (as previously defined for cat, dog, bird)
-exclude_classes = cat_classes + dog_classes + bird_classes
-
-# Filter out the excluded subclasses from the main animal list
-remaining_animal_classes = [animal for animal in animal_classes if animal not in exclude_classes]
-
-# Count the remaining animal classes
-print("Number of animal classes excluding cats, dogs, and birds:", len(remaining_animal_classes))
-
-# print("Number of cat subclasses:", len(cat_classes))
-# print("Number of dog subclasses:", len(dog_classes))
-# print("Number of bird subclasses:", len(bird_classes))
-print(len(dog_classes) + len(cat_classes) + len(bird_classes))
+items = [
+    {
+        "category": "Flowers",
+        "items": ["Daisy", "Yellow lady's slipper"]
+    },
+    {
+        "category": "Fruits",
+        "items": ["Granny Smith", "Strawberry", "Orange", "Lemon", "Fig", "Pineapple", "Banana", "Jackfruit", "Custard apple", "Pomegranate"]
+    },
+    {
+        "category": "Vegetables",
+        "items": ["Zucchini", "Broccoli", "Cauliflower"]
+    },
+    {
+        "category": "Vehicles",
+        "items": ["Motorcycle", "Truck", "Bus"]
+    }
+]
--- ai/vision/Deeper_RelTR/second_classification/person_processing.py
+++ ai/vision/Deeper_RelTR/second_classification/person_processing.py
@@ -18,17 +18,17 @@ def person_processing(x_min, y_min, x_max, y_max, image, i):
     cropped_image_cv = cv2.cvtColor(np.array(cropped_image), cv2.COLOR_RGB2BGR)
 
     try:
-        analysis_results = DeepFace.analyze(img_path=cropped_image_cv, actions=['age', 'gender', 'race', 'emotion'], enforce_detection=False)
+        analysis_results = DeepFace.analyze(img_path=cropped_image_cv, actions=['age', 'race', 'emotion'], enforce_detection=False)
         analysis = analysis_results[0] if analysis_results else {}
         
-        print(f"Analysis for person {i+1}:")
-        print(f"Age: {analysis.get('age', 'N/A')}")
-        print(f"Gender: {analysis.get('dominant_gender', 'N/A')}")
-        print(f"Race: {analysis.get('dominant_race', 'N/A')}")
-        print(f"Emotion: {analysis.get('dominant_emotion', 'N/A')}")
+        # print(f"Analysis for person {i+1}:")
+        # print(f"Age: {analysis.get('age', 'N/A')}")
+        # print(f"Gender: {analysis.get('dominant_gender', 'N/A')}")
+        # print(f"Race: {analysis.get('dominant_race', 'N/A')}")
+        # print(f"Emotion: {analysis.get('dominant_emotion', 'N/A')}")
 
         person_string = str(analysis.get('age', 'N/A'))+" year old "+ str(analysis.get('dominant_emotion', 'N/A'))+ " "+\
-                        str(analysis.get('dominant_race', 'N/A')) +" "+ str(analysis.get('dominant_gender', 'N/A'))
+                        str(analysis.get('dominant_race', 'N/A')) +" "
         return person_string
     except Exception as e:
         print(f"An error occurred during analysis: {e}")
