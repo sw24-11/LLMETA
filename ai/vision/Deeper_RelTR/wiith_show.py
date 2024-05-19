26214cc949a40dd585bc8efc441fd5d4443ffbe3
KBH00 <wlfjddlgoqn@knu.ac.kr>
2024-05-19 10:04:18 +0900

    add api format

--- ai/vision/Deeper_RelTR/wiith_show.py
+++ ai/vision/Deeper_RelTR/wiith_show.py
@@ -239,7 +239,7 @@ def get_args_parser():
     parser.add_argument('--giou_loss_coef', default=2, type=float)
     parser.add_argument('--rel_loss_coef', default=1, type=float)
     parser.add_argument('--eos_coef', default=0.1, type=float,
-                        help="Relative classification weight of the no-object class")
+                        help="Relative classification weight of the no-object class") 
 
 
     # distributed training parameters
