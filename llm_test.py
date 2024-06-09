4e7e0bc9de46d753a1c9ea772623224c5b1162f7
KBH00 <wlfjddlgoqn@knu.ac.kr>
2024-06-09 19:26:55 +0900

    prompting

--- llm_test.py
+++ llm_test.py
@@ -60,15 +60,14 @@ class llama2_chain:
     def llm_set(self):
         metadata_extraction_template = """
 
-        Answer only once with this format and do not say anything else. 
+        Answer with this format from research paper and do not say anything else. 
 
         Title:
         Authors:
         Abstract:
         Research Domain:
 
-
-        Actual Text: {text}
+        research paper is {text}
         """
 
         prompt_template = PromptTemplate(template=metadata_extraction_template, input_variables=["text"])
@@ -88,3 +87,53 @@ class llama2_chain:
 
         prompt = prompt_template.format(text=self.input_paper)
         return llm_chain, prompt
+
+def truncate_text(text, max_length=3072):
+    words = text.split()
+    truncated_text = ' '.join(words[:max_length])
+    return truncated_text
+
+import re
+import fitz
+
+def extract_text_from_pdf(file_path):
+    document = fitz.open(file_path)
+    text = ''
+    for page in document:
+        text += page.get_text("text")
+    return text
+
+def extract_before_introduction(text):
+    abstract_pattern = re.compile(r'abstract', re.IGNORECASE)
+    introduction_pattern = re.compile(r'introduction', re.IGNORECASE)
+    reference_pattern = re.compile(r'reference', re.IGNORECASE)
+
+    abstract_match = abstract_pattern.search(text)
+    if not abstract_match:
+        return "Abstract section not found"
+
+    introduction_match = introduction_pattern.search(text, abstract_match.end())
+    if not introduction_match:
+        return text[0:abstract_match.start()]
+
+    return text[0:introduction_match.start()]
+
+def text_inference(research_paper):
+    model_paths = ["C:/Users/kbh/Code/project2/llm/models/llama-2-7b-chat.Q2_K.gguf"]
+    n_batches = [2000, 3000, 4000]
+    n_gpu = 50
+
+    # Truncate text to fit within the model's token limit
+    truncated_paper = extract_before_introduction(research_paper)
+
+    llm = llama2_chain(model_paths[0], n_batch=n_batches[0], n_gpu_layers=n_gpu,
+                       input_paper=truncated_paper, cb_manager=0)
+    llm_chain, prompt = llm.llm_set()
+    response = llm_chain.invoke(prompt)
+    info_dict = extract_info(response['text'])
+    return info_dict
+
+text_content = extract_text_from_pdf("C:/Users/kbh/Code/project2/vision/LLMETA/ai/uploads/paper.pdf")
+metadata = text_inference(text_content)
+merged_list = [{"key": key, "value": value} for key, value in metadata.items()]
+print(merged_list)
